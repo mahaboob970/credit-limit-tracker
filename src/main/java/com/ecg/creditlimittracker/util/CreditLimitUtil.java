45f685b220bc9c3c8096b7a0189d4243d96e69e8
mahaboob970 <95222273+mahaboob970@users.noreply.github.com>
2022-02-22 11:07:23 +0530

    Update CreditLimitUtil.java

--- src/main/java/com/ecg/creditlimittracker/util/CreditLimitUtil.java
+++ src/main/java/com/ecg/creditlimittracker/util/CreditLimitUtil.java
@@ -1,9 +1,11 @@
 package com.ecg.creditlimittracker.util;
 
+import org.springframework.util.StringUtils;
 
 public class CreditLimitUtil {
 
     public static double convertDollarToCent(String dollar) {
+    	if(StringUtils.isEmpty(dollar)) return 0.0;
         return (Double.parseDouble(dollar) / 100);
     }
 }
